9c08105179cb14c0b1d7a7fd36d036aacc47f178
Thibaut Crenn <crenn@freshplanet.com>
2013-03-11 19:28:08 -0400

    adds subscription interface for Amazon

---
+++ NativeAmazon/src/com/freshplanet/inapppurchase/UserCanMakeASubscriptionFunction.java
@@ -0,0 +1,15 @@
+package com.freshplanet.inapppurchase;
+
+import com.adobe.fre.FREContext;
+import com.adobe.fre.FREFunction;
+import com.adobe.fre.FREObject;
+
+public class UserCanMakeASubscriptionFunction implements FREFunction {
+
+	@Override
+	public FREObject call(FREContext arg0, FREObject[] arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
